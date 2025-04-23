161f944037b6c9834754c951f3424fe2b31aa249
Jesus <jsr20xx@gmail.com>
2025-04-23 22:33:31 +0200

    add app config

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+import os
+from .routes import routes
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config['UPLOAD_FOLDER'] = os.path.join('app', 'static', 'images')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    app.register_blueprint(routes)
 
     with app.app_context():
-        from . import routes
         db.create_all()
 
     return app
