4a00c7c8e23fe01e969bd078df7324e07ceb0d47
Jesus <jsr20xx@gmail.com>
2025-05-04 13:18:03 +0200

    Allow only frontend domain and add gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app) 
+    CORS(app, origins="https://app.jsanr.dev") 
     app.config['UPLOAD_FOLDER'] = os.path.join('app', 'static', 'images')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
